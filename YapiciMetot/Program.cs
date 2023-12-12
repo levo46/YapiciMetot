f129224f662c685aebf1f24ca823cf52a30a02b8
levent581 <levent581@BPC03>
2023-12-12 08:42:37 +0300

    Proje dosyası ekle.

---
+++ YapiciMetot/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YapiciMetot
+{ 
+    class Kisi
+    {
+        int yas;
+        string ad;
+        public Kisi() 
+        {
+            yas = 15;
+            ad = "Levent";
+            Console.WriteLine("Yapıcı Metot Çalıştıtır");
+        }
+        public int Yas 
+        {
+         get { return yas; }    
+        }
+        public string Ad
+        {
+            get { return ad; }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pogram başladı.");
+            Kisi K = new Kisi();
+            Console.WriteLine("ad:{0},Yaşı:{0}",K.Ad,K.Yas);
+        }
+    }
+}
